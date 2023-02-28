968b04f0f3a45a59f3e66073957b36cbfe2f3c2a
Vlad-Kolyada <107812458+daddy-our-bandera@users.noreply.github.com>
2023-02-28 12:42:45 +0200

    fix_props

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,8 @@
-import { useSelector } from 'react-redux';
-
 import AddForm from 'components/AddForm/AddForm';
 import { ContactList } from './ContactList/ContactList';
-import { Filter } from './Filter/Filter';
 import { Section } from './Section/Section.styled';
-import { getContacts, getFilter } from 'redux/selectors';
 
 function App() {
-  const contacts = useSelector(getContacts);
-  const filter = useSelector(getFilter);
-
-  const normalizedFilter = filter.toLowerCase();
-  const filterContacts = contacts.filter(contact =>
-    contact.name.toLowerCase().includes(normalizedFilter)
-  );
-
   return (
     <div>
       <Section>
@@ -24,14 +12,7 @@ function App() {
 
       <Section>
         <h2>Contacts</h2>
-
-        {filterContacts.length > 0 || filter ? (
-          <Filter value={filter} />
-        ) : (
-          <h1>No contacts added</h1>
-        )}
-
-        <ContactList contacts={filterContacts} />
+        <ContactList />
       </Section>
     </div>
   );
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,23 +1,31 @@
-import PropTypes from 'prop-types';
 import { Item } from 'components/ContactListItem/ContactListItem';
 import { List } from './ContactList.styled';
+import { useSelector } from 'react-redux';
+import { getContacts, getFilter } from 'redux/selectors';
+import { Filter } from 'components/Filter/Filter';
+import { Section } from 'components/Section/Section.styled';
 
-export const ContactList = ({ contacts }) => {
-  return (
-    <List>
-      {contacts.map(({ id, name, number }) => (
-        <Item key={id} id={id} name={name} number={number} />
-      ))}
-    </List>
+export const ContactList = () => {
+  const contacts = useSelector(getContacts);
+  const filter = useSelector(getFilter);
+
+  const normalizedFilter = filter.toLowerCase();
+  const filterContacts = contacts.filter(contact =>
+    contact.name.toLowerCase().includes(normalizedFilter)
   );
-};
 
-ContactList.propTypes = {
-  contacts: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.string.isRequired,
-      name: PropTypes.string.isRequired,
-      number: PropTypes.string.isRequired,
-    })
-  ),
+  return (
+    <Section>
+      {filterContacts.length > 0 || filter ? (
+        <Filter value={filter} />
+      ) : (
+        <h1>No contacts added</h1>
+      )}
+      <List>
+        {filterContacts.map(({ id, name, number }) => (
+          <Item key={id} id={id} name={name} number={number} />
+        ))}
+      </List>
+    </Section>
+  );
 };
